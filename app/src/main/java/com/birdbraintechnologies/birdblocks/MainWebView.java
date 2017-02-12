70a4765da41d9d176910e42cba475a49229f27dc
Terence Sun <tsun1215@gmail.com>
2017-02-12 18:46:53 -0500

    Changed min sdk version to allow for immersive UI for webview

--- app/src/main/java/com/birdbraintechnologies/birdblocks/MainWebView.java
+++ app/src/main/java/com/birdbraintechnologies/birdblocks/MainWebView.java
@@ -2,6 +2,7 @@ package com.birdbraintechnologies.birdblocks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -11,8 +12,11 @@ public class MainWebView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_web_view);
-        WebView myWebView = (WebView) findViewById(R.id.main_webview);
-        WebSettings webSettings = myWebView.getSettings();
+        WebView wv = (WebView) findViewById(R.id.main_webview);
+        wv.loadUrl("file:///android_asset/frontend/HummingbirdDragAndDrop.html");
+        wv.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        WebSettings webSettings = wv.getSettings();
         webSettings.setJavaScriptEnabled(true);
     }
 }
