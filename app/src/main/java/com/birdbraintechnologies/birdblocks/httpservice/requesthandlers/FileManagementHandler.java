77af98bcaa487498ab71d47f9387b8afa1cd7886
Terence Sun <tsun1215@gmail.com>
2017-02-27 23:54:22 -0500

    Added comments for FileManagementHandler

--- app/src/main/java/com/birdbraintechnologies/birdblocks/httpservice/requesthandlers/FileManagementHandler.java
+++ app/src/main/java/com/birdbraintechnologies/birdblocks/httpservice/requesthandlers/FileManagementHandler.java
@@ -7,7 +7,6 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.birdbraintechnologies.birdblocks.MainWebView;
-import com.birdbraintechnologies.birdblocks.dialogs.BirdblocksDialog;
 import com.birdbraintechnologies.birdblocks.httpservice.HttpService;
 import com.birdbraintechnologies.birdblocks.httpservice.RequestHandler;
 
@@ -17,8 +16,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,9 +23,10 @@ import java.util.Map;
 import fi.iki.elonen.NanoHTTPD;
 
 /**
- * Created by tsun on 2/23/17.
+ * Request handler for managing files on the device.
+ *
+ * @author Terence Sun (tsun1215)
  */
-
 public class FileManagementHandler implements RequestHandler {
     private static final String TAG = FileManagementHandler.class.getName();
     private static final String BIRDBLOCKS_SAVE_DIR = "Birdblocks";
@@ -72,6 +70,13 @@ public class FileManagementHandler implements RequestHandler {
         return r;
     }
 
+
+    /**
+     * Saves a file to the device (Only supports POST requests)
+     *
+     * @param filename Name of the file to save
+     * @param session  HttpRequest to get the POST body of
+     */
     private void saveFile(String filename, NanoHTTPD.IHTTPSession session) {
         if (session.getMethod() != NanoHTTPD.Method.POST) {
             Log.d(TAG, "Save must be done via POST request");
@@ -80,10 +85,10 @@ public class FileManagementHandler implements RequestHandler {
         Map<String, String> postFiles = new HashMap<>();
         File newFile = new File(getBirdblocksDir(), filename);
         try {
-            // Parse post body to get parameters
+            // Parse POST body to get parameters
             session.parseBody(postFiles);
 
-            // Write "data" to file
+            // Write POST["data"] to file
             FileWriter writer = new FileWriter(newFile);
             writer.write(session.getParameters().get("data").get(0));
             writer.close();
@@ -95,6 +100,12 @@ public class FileManagementHandler implements RequestHandler {
         }
     }
 
+    /**
+     * Loads a file from the device
+     *
+     * @param filename Name of the file to load
+     * @return String contents of the file
+     */
     private String loadFile(String filename) {
         File file = new File(getBirdblocksDir(), filename);
         if (!file.exists()) {
@@ -104,7 +115,7 @@ public class FileManagementHandler implements RequestHandler {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
             String line;
-            while((line = reader.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
                 response.append(line + "\n");
             }
         } catch (FileNotFoundException e) {
@@ -115,6 +126,12 @@ public class FileManagementHandler implements RequestHandler {
         return response.toString().trim();
     }
 
+    /**
+     * Renames a saved file on the device
+     *
+     * @param oldFilename Old file name
+     * @param newFilename New name of file
+     */
     private void renameFile(String oldFilename, String newFilename) {
         File file = new File(getBirdblocksDir(), oldFilename);
         if (!file.exists()) {
@@ -123,6 +140,11 @@ public class FileManagementHandler implements RequestHandler {
         file.renameTo(new File(getBirdblocksDir(), newFilename));
     }
 
+    /**
+     * Deletes a saved file on the device
+     *
+     * @param filename Name of file to delete
+     */
     private void deleteFile(String filename) {
         File file = new File(getBirdblocksDir(), filename);
         if (!file.exists()) {
@@ -131,6 +153,11 @@ public class FileManagementHandler implements RequestHandler {
         file.delete();
     }
 
+    /**
+     * Lists the files on the device
+     *
+     * @return List of files on the device separated by \n
+     */
     private String listFiles() {
         File[] files = getBirdblocksDir().listFiles();
         String response = "";
@@ -143,8 +170,17 @@ public class FileManagementHandler implements RequestHandler {
         return response;
     }
 
+    /**
+     * Starts a share command for a saved file on the device
+     *
+     * @param filename Name of the file to share
+     * @param session  HttpRequest containing the most up to date contents of the file
+     */
     private void exportFile(String filename, NanoHTTPD.IHTTPSession session) {
+        // Save the updated contents (in case they were updated)
         saveFile(filename, session);
+
+        // Create share intent on the main activity
         File file = new File(getBirdblocksDir(), filename);
         if (file.exists()) {
             Intent showDialog = new Intent(MainWebView.SHARE_FILE);
@@ -153,8 +189,12 @@ public class FileManagementHandler implements RequestHandler {
         }
     }
 
+    /**
+     * Gets the BirdBlocks save directory
+     *
+     * @return File object for the save directory
+     */
     private File getBirdblocksDir() {
-        // Get the directory for the user's public pictures directory.
         File file = new File(Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_DOCUMENTS), BIRDBLOCKS_SAVE_DIR);
         if (!file.mkdirs()) {
