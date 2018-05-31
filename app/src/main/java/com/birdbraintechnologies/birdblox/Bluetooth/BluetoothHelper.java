19ee2b6bb72d2e6519026813930c03f19e012032
yzd1998111 <1052703078@qq.com>
2018-05-31 10:34:56 -0400

    commit

--- app/src/main/java/com/birdbraintechnologies/birdblox/Bluetooth/BluetoothHelper.java
+++ app/src/main/java/com/birdbraintechnologies/birdblox/Bluetooth/BluetoothHelper.java
@@ -1,5 +1,4 @@
 package com.birdbraintechnologies.birdblox.Bluetooth;
-
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
@@ -12,18 +11,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.util.Log;
-
 import com.birdbraintechnologies.birdblox.Robots.RobotType;
 import com.birdbraintechnologies.birdblox.Util.NamingHandler;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
 import static com.birdbraintechnologies.birdblox.MainWebView.bbxEncode;
 import static com.birdbraintechnologies.birdblox.MainWebView.mainWebViewContext;
 import static com.birdbraintechnologies.birdblox.MainWebView.runJavascript;
@@ -31,7 +26,6 @@ import static com.birdbraintechnologies.birdblox.httpservice.RequestHandlers.Rob
 import static com.birdbraintechnologies.birdblox.httpservice.RequestHandlers.RobotRequestHandler.fluttersToConnect;
 import static com.birdbraintechnologies.birdblox.httpservice.RequestHandlers.RobotRequestHandler.hummingbirdsToConnect;
 import static com.birdbraintechnologies.birdblox.httpservice.RequestHandlers.RobotRequestHandler.lastScanType;
-
 /**
  * Helper class for basic Bluetooth connectivity
  *
@@ -47,7 +41,7 @@ public class BluetoothHelper {
     private Context context;
     public static HashMap<String, BluetoothDevice> deviceList;
     private BluetoothLeScanner scanner;
-
+    private static HashMap<String, Integer> deviceRSSI = new HashMap<>();
     /* Callback for populating the device list */
     private ScanCallback populateDevices = new ScanCallback() {
         @Override
@@ -64,13 +58,16 @@ public class BluetoothHelper {
                         connectToRobot(RobotType.Flutter, result.getDevice().getAddress());
                     }
                 }
+                deviceRSSI.put(result.getDevice().getAddress(),result.getRssi());
                 JSONArray robots = new JSONArray();
                 for (BluetoothDevice device : BLEDeviceList) {
                     String name = NamingHandler.GenerateName(mainWebViewContext.getApplicationContext(), device.getAddress());
+                    String prefix = device.getName();
                     JSONObject robot = new JSONObject();
                     try {
                         robot.put("id", device.getAddress());
                         robot.put("name", name);
+                        robot.put("RSSI", deviceRSSI.get(device.getAddress()));
                     } catch (JSONException e) {
                         Log.e("JSON", "JSONException while discovering " + lastScanType);
                     }
@@ -80,7 +77,6 @@ public class BluetoothHelper {
             }
         }
     };
-
     /**
      * Initializes a Bluetooth helper
      *
@@ -91,12 +87,10 @@ public class BluetoothHelper {
         this.btScanning = false;
         this.handler = new Handler();
         deviceList = new HashMap<>();
-
         // Acquire Bluetooth service
         final BluetoothManager btManager =
                 (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
         this.btAdapter = btManager.getAdapter();
-
         // Ask to enable Bluetooth if disabled
         if (!btAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
@@ -104,7 +98,6 @@ public class BluetoothHelper {
             context.startActivity(enableBtIntent);
         }
     }
-
     /**
      * Scans for Bluetooth devices that matches the filter.
      *
@@ -144,7 +137,6 @@ public class BluetoothHelper {
             currentlyScanning = true;
         }
     }
-
     /**
      * Connects to a device and returns the resulting connection
      *
@@ -161,12 +153,9 @@ public class BluetoothHelper {
             Log.e(TAG, "Unable to connect to device: " + addr);
             return null;
         }
-
         UARTConnection conn = new UARTConnection(context, device, settings);
-
         return conn;
     }
-
     /**
      * Connects to a device and returns the resulting connection
      *
@@ -180,12 +169,9 @@ public class BluetoothHelper {
             Log.e(TAG, "Unable to connect to device: " + addr);
             return null;
         }
-
         MelodySmartConnection conn = new MelodySmartConnection(context, device, settings);
-
         return conn;
     }
-
     public void stopScan() {
         if (scanner != null) {
             scanner.stopScan(populateDevices);
@@ -196,7 +182,5 @@ public class BluetoothHelper {
             deviceList.clear();
         }
         currentlyScanning = false;
-
     }
-
 }
