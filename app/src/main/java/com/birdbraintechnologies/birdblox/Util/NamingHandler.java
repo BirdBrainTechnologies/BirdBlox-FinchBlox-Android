34b6c7393d15cc6a8c13b20c5a0750faf9adfc03
yzd1998111 <1052703078@qq.com>
2018-07-06 10:38:06 -0400

    badNames Eliminated

--- app/src/main/java/com/birdbraintechnologies/birdblox/Util/NamingHandler.java
+++ app/src/main/java/com/birdbraintechnologies/birdblox/Util/NamingHandler.java
@@ -5,6 +5,9 @@ import android.content.Context;
 
 import com.birdbraintechnologies.birdblox.R;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Steve on 6/9/2016.
  *
@@ -19,17 +22,20 @@ public class NamingHandler {
         if (mac == null) return "";
         String result = "unknown";
         long mid;
-        int i, j, k, offset;
-        String[] firstNames,middleNames,lastNames;
+        int i, j, k, offset, badWordsLen;
+        String[] firstNames,middleNames,lastNames, badNames;
         firstNames = context.getResources().getStringArray(R.array.first_names);
         middleNames = context.getResources().getStringArray(R.array.middle_names);
         lastNames = context.getResources().getStringArray(R.array.last_names);
-
+        badNames = context.getResources().getStringArray(R.array.bad_names);
+        badWordsLen = badNames.length;
+        List<String> badNamesList = Arrays.asList(badNames);
         // expected input: "aa:bb:cc:dd:ee:ff" => "d:ee:ff"
         mac = mac.substring(9);
         String first;
         String middle;
         String last;
+        String prefix;
 
         // grab bits from the MAC address (6 bits, 6 bits, 8 bits => last, middle, first)
         i = Integer.parseInt(mac.substring(6),16);
@@ -48,13 +54,20 @@ public class NamingHandler {
         last = lastNames[k];
 
         if (first != null && middle != null && last != null) {
+            prefix = Character.toString(first.charAt(0)) + Character.toString(middle.charAt(0)) +
+                     Character.toString(last.charAt(0));
+            while (badNamesList.contains(prefix)) {
+                j = (j + 1) % badWordsLen;
+                middle = middleNames[j];
+                prefix = Character.toString(first.charAt(0)) + Character.toString(middle.charAt(0)) +
+                         Character.toString(last.charAt(0));
+            }
             first = first.concat(" ");
             result = result.replace(result, first);
             middle = middle.concat(" ");
             result = result.concat(middle);
             result = result.concat(last);
         }
-
         return result;
     }
 
